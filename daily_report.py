e236c9244330ff952cb3c06401537214be31ad9f
CharlesGuo <gchaozheng@gmail.com>
2020-04-25 11:58:36 +0800

    update

--- daily_report.py
+++ daily_report.py
@@ -63,10 +63,13 @@ def main(argv):
             #Enter daily temperature
             temp = round(random.uniform(36, 37), 1)
             temp_input = driver.find_element_by_xpath("//input[@data-name='DZ_JSDTCJTW']")
-            temp_input.send_keys(str(temp))            
+            driver.execute_script("arguments[0].value = arguments[1];", temp_input, str(temp))
+            # temp_input.send_keys(str(temp))            
             # time.sleep(30)
             # print(temp)
             print("Daily temperature: %s" % temp_input.get_attribute('value'))
+            # driver.execute_script("document.getElementById('...').setAttribute('value', 'SM37')")
+            exit()
 
             # Save data
             driver.find_element_by_xpath("//div[@data-action='save']").click()
