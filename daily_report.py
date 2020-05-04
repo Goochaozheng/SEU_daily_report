2b048a66bc8571616b23fa7c9a70f5ccfc9d9f74
CharlesGuo <gchaozheng@gmail.com>
2020-05-04 13:23:18 +0800

    add retry times

--- daily_report.py
+++ daily_report.py
@@ -8,6 +8,9 @@ import random
 import time
 import traceback
 
+RETRY_TIMES = 10
+WAIT_TIME = 60
+
 def main(argv):
 
     if len(argv) != 4:
@@ -35,23 +38,23 @@ def main(argv):
     driver.get("http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/index.do?t_s=1583651212299#/dailyReport")
 
     print("Enter login page")
-    WebDriverWait(driver, 60, 1).until(expected_conditions.visibility_of_element_located((By.ID, "username")))
+    WebDriverWait(driver, WAIT_TIME, 1).until(expected_conditions.visibility_of_element_located((By.ID, "username")))
 
     driver.find_element_by_id("username").send_keys(userid)
     driver.find_element_by_id("password").send_keys(password)
     driver.find_element_by_xpath("//button[@type='submit']").click()
 
     #Refresh if timeout or exception
-    for i in range(5):
+    for i in range(RETRY_TIMES):
         try:
-            WebDriverWait(driver, 60, 1).until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@data-action='add']")))
+            WebDriverWait(driver, WAIT_TIME, 1).until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@data-action='add']")))
             print("Login successfully as %s" % userid)
 
             #Add data
             driver.find_element_by_xpath("//div[@data-action='add']").click()
 
             try:
-                WebDriverWait(driver, 60, 1).until(expected_conditions.visibility_of_element_located((By.XPATH, "//p[@data-name='USER_ID']")))
+                WebDriverWait(driver, WAIT_TIME, 1).until(expected_conditions.visibility_of_element_located((By.XPATH, "//p[@data-name='USER_ID']")))
             except Exception as e:
                 if expected_conditions.visibility_of_element_located((By.XPATH, "//a[@class='bh-dialog-btn bh-bg-primary bh-color-primary-5']")):
                     print("Already reported")
@@ -61,7 +64,7 @@ def main(argv):
             print("Adding data")
 
             #Enter daily temperature
-            temp = round(random.uniform(36, 37), 1)
+            temp = round(random.uniform(36, 36.9), 1)
             temp_input = driver.find_element_by_xpath("//input[@data-name='DZ_JSDTCJTW']")
             driver.execute_script("arguments[0].value = arguments[1];", temp_input, str(temp))
             # temp_input.send_keys(str(temp))            
@@ -70,7 +73,7 @@ def main(argv):
 
             # Save data
             driver.find_element_by_xpath("//div[@data-action='save']").click()
-            WebDriverWait(driver, 60, 1).until(expected_conditions.visibility_of_element_located((By.XPATH, "//a[@class='bh-dialog-btn bh-bg-primary bh-color-primary-5']")))
+            WebDriverWait(driver, WAIT_TIME, 1).until(expected_conditions.visibility_of_element_located((By.XPATH, "//a[@class='bh-dialog-btn bh-bg-primary bh-color-primary-5']")))
             driver.find_element_by_xpath("//a[@class='bh-dialog-btn bh-bg-primary bh-color-primary-5']").click()
             print("Save success")
             sys.exit()
